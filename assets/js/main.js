11531ea80a843aa032c0566daf7d1c002803dc1f
Valentino Gechev <valentinogechev@gmail.com>
2020-08-30 23:20:06 +0300

    added jquery

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(function () {
+    var navbar = $(".desktop-nav");
+
+    $(document).scroll(function () {        
+        navbar.toggleClass('scrolled', $(this).scrollTop() > navbar.height());
+    });
+
+    $(navbar).hover(function () {        
+        if (window.scrollY === 0) {
+            navbar.removeClass('hovered');            
+        }
+        else {
+            navbar.toggleClass('hovered');
+        }        
+    });
+});
